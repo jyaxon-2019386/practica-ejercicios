314e40a527a6dd1723469e98be576e28840fb10b
jyaxon-2019386 <134967084+jyaxon-2019386@users.noreply.github.com>
2024-09-20 14:53:04 -0600

    Add files via upload

--- php/middlewares/validator.php
+++ php/middlewares/validator.php
@@ -1,5 +1,5 @@
 <?php
-    // --------- VALIDACIONES DE AUTENTICACION --------- //
+    // --------- VALIDACIONES DE CARACTERES INPUTS --------- //
 
     function validar_usuario($res){
         return preg_match("/^[a-zA-Z0-9_]{3,20}$/", $res);
@@ -11,5 +11,15 @@
 
     function validar_contrasena($res){
         return preg_match("/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,64}$/", $res);
+    }
+
+    // Funciones para UPDATE
+
+    function validar_contrasena_nueva($res) {
+        return empty($res) || preg_match("/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,64}$/", $res);
+    }
+    
+    function validar_repetir_contrasena_nueva($res) {
+        return empty($res) || preg_match("/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,64}$/", $res);
     }
 ?>
